71aeb87f1d890d6e153767cb774f0dfdcc78da89
tristankoehler01 <126407283+tristankoehler01@users.noreply.github.com>
2023-02-26 10:50:52 -0600

    Edits
    Made database show up on the home screen

--- GTAWebsite/Pages/Index.cshtml.cs
+++ GTAWebsite/Pages/Index.cshtml.cs
@@ -10,18 +10,23 @@ namespace GTAWebsite.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly GTAWebsite.Data.GTAWebsiteContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, GTAWebsite.Data.GTAWebsiteContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public void OnGet()
+        public IList<Course> Course { get; set; } = default!;
+
+        public async Task OnGetAsync()
         {
-
+            if (_context.Course != null)
+            {
+                Course = await _context.Course.ToListAsync();
+            }
         }
-
-        public List<Course> courses { get; set; } = default!;
 
     }
 }
