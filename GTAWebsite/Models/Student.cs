aa409e6437a69153ac7c1f2093d4a696c7cb369e
adeesha-naveed <77710441+adeesha-naveed@users.noreply.github.com>
2023-02-28 15:22:21 -0600

    FileUpload

---
+++ GTAWebsite/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace GTAWebsite.Models
+{
+    public class Student : User
+    {
+        public double GPA { get; set; }
+        public bool GTACertified { get; set; }
+        public string Degree { get; set; }
+
+    }
+}
