bf5bdfad599b37c9f9ee3884c559c8b06a3aea63
tristankoehler01 <126407283+tristankoehler01@users.noreply.github.com>
2023-03-14 14:52:43 -0500

    Created more page and am linking it to form.

---
+++ GTAWebsite/Pages/More.cshtml.cs
@@ -0,0 +1,13 @@
+using GTAWebsite.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GTAWebsite.Pages
+{
+    public class MoreModel : PageModel
+    { 
+        public void OnGet()
+        {
+        }
+    }
+}
